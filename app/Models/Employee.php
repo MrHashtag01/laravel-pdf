83cc75f838d8abc74d037726ea0ace289836b86d
Hashaam Sheikh <hashaamsheikh@hotmail.co.uk>
2023-05-14 18:00:48 +0400

    Added code to model

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -2,10 +2,7 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
-class Employee extends Model
-{
-    use HasFactory;
+class Employee extends Model {
+    public $fillable = ['name', 'email', 'phone_number', 'dob'];
 }
